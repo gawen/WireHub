5207b86326a2e962cb6212f854d0d961777b37e9
Gawen Arab <g@wenarab.com>
2018-12-29 00:46:33 +0100

    testbed: use helpers to initialize network with bootstrap

--- tests/testbed/test_02_simple.py
+++ tests/testbed/test_02_simple.py
@@ -4,53 +4,43 @@ def test_ping():
     unet = """
     W = wan()
     M(W | peer{up_ip=subnet('1.1.1.1', 0)})
-    M(W | peer{up_ip=subnet('1.1.1.2', 0)})
-    M(W | peer{up_ip=subnet('1.1.1.3', 0)})
+
+    for i = 1, 4 do
+        M(W | peer{})
+    end
     """
 
-    net = "public"
-
     with env(unet) as e:
-        # setup network
-        for n in e.nodes.values():
-            with n.wh() as wh:
-                wh("clearconf", "public")
-                wh("set", "public", workbit=8)
-
-        # generate keys
-        keys = {}
-        for peer_id, n in e.nodes.items():
-            with n.wh() as wh:
-                sk, k = keys[peer_id] = wh.genkey("public")
-                wh.sh(f"echo {sk} > /sk")
-
-        # setup bootstrap
-        for peer_id, n in e.nodes.items():
-            with n.wh() as wh:
-                wh("set", "public", endpoint="1.1.1.1", bootstrap="yes", untrusted=True, peer=keys[1][1])
+        e.setup_public()
 
         # start daemon
-        whs = {}
         for peer_id, n in e.nodes.items():
-            wh = whs[peer_id] = n.wh()
-            k = keys[peer_id][1]
-
-            cmd = ["up", "public", "private-key", "/sk"]
             if peer_id == 1:
-                cmd.extend(("mode", "direct"))
+                continue
 
-            wh(*cmd, blocking=False)
+            wh = n.daemon_wh = n.wh()
 
-            @retry()
-            def f():
-                return wh.inspect(k) != None
+            wh("up", "public", "private-key", "/sk", blocking=False)
 
-        # get ipc
+        with e.nodes[1].wh() as wh:
+            _, fake_k = wh.genkey("public")
+
         for peer_id, n in e.nodes.items():
-            k = keys[peer_id][1]
+            if peer_id == 1:
+                continue
 
             with n.wh() as wh:
-                #print("all", repr(wh("show", "all")))
-                print("specific", repr(wh.inspect(k)))
+                @retry()
+                def f():
+                    return wh.inspect(n.k) != None
 
+                for other_id, other_n in e.nodes.items():
+                    if peer_id == other_id:
+                        continue
+
+                    wh('ping', n.k, other_n.k)
+                    assert(wh.sh.value == 0)
+
+                wh('ping', n.k, fake_k)
+                assert wh.sh.value != 0
 
