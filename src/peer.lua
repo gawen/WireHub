f3110b1b03717faf3e06a9425d2fd071c4335edb
Gawen Arab <g@wenarab.com>
2019-01-10 19:55:09 +0100

    Add 'acquire', 'release' and 'state' to peers

--- src/peer.lua
+++ src/peer.lua
@@ -51,6 +51,66 @@ function MT.__tostring(p)
     return s
 end
 
+-- Returns two values:
+-- 1. the type of the peer
+-- 2. if it is considered as active
+function MT.__index.state(p)
+    -- peer is an alias: a public-key of a private key given to the peer to
+    -- authenticate in the future
+    if p.alias then
+        return 'alias', false
+
+    -- peer may be contacted through a relay
+    elseif p.relay then
+        return 'relay', now-(p.last_seen or 0) <= wh.KEEPALIVE_NAT_TIMEOUT
+
+    -- peer is connected in P2P, but behind a NAT
+    elseif p.is_nated and p.addr then
+        return 'nat', now-(p.last_seen or 0) <= wh.KEEPALIVE_NAT_TIMEOUT
+
+    -- peer is direct
+    elseif p.addr then
+        return 'direct', now-(p.last_seen or 0) <= wh.KEEPALIVE_DIRECT_TIMEOUT
+
+    -- no connection information
+    else
+        return nil, false
+    end
+end
+
+function MT.__index.acquire(p, obj)
+    if p.ref == nil then
+        p.ref = {}
+    end
+
+    p.ref[obj] = true
+
+    --dbg('acquire %s', dump(p))
+    return p
+end
+
+function MT.__index.release(p, obj)
+    if p.ref == nil then
+        return
+    end
+
+    p.ref[obj] = nil
+
+    local empty = true
+    for _ in ipairs(p.ref) do
+        empty = false
+        break
+    end
+
+    if empty then
+        p.ref = nil
+    end
+end
+
+function MT.__index.owned(p)
+    return not not p.ref
+end
+
 return function(p)
     return setmetatable(p, MT)
 end
